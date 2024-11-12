a4ca1e9e3ac647ee9179cbe41be8aab6f9505c01
Rchongky <ryancth1907@gmail.com>
2024-11-12 15:50:00 +0800

    commit

--- spoweb/src/App.js
+++ spoweb/src/App.js
@@ -10,21 +10,28 @@ import SashaResume from './SashaResume';
 
 
 const App = () => {
-  const [isCaptchaOpen, setIsCaptchaOpen] = useState(false);
-  const [isVerified, setIsVerified] = useState(false);
-  const [ipInfo, setIpInfo] = useState(null);
-  const [webrtcIps, setWebrtcIps] = useState([]);
-  const [vpnStatus, setVpnStatus] = useState(null);
-  const [ipScore, setIpScore] = useState(null);
+  const [isCaptchaOpen, setIsCaptchaOpen] = useState(false); // const[orginal state, function to update state] = (currently set as 'false')
+  const [isVerified, setIsVerified] = useState(false); // check if captcha is completed 
+  const [ipInfo, setIpInfo] = useState(null); // perform IP info extraction using IP info API
+  const [webrtcIps, setWebrtcIps] = useState([]); // Check for webRTC IP 
+  const [vpnStatus, setVpnStatus] = useState(null); // Check for VPN status 
+  const [ipScore, setIpScore] = useState(null); // Check for IP score 
 
 
-  const backendURL = 'https://spoweb-test.onrender.com';
+  const backendURL = 'https://spoweb-test.onrender.com'; // Indicate where to send the backend messages to 
   // const backendURL = 'https://localhost:4000';
 
   // backend logging function 
   const logToBackend = async (message) => {
     try {
-      console.log(message);
+      //console.log(message); // call for all message that is log to backend to be shown on console 
+
+      //let encodedMessage = btoa(message) //encoding all the message to backend 
+      //console.log("e_messaage =", encodedMessage) // check on console for encoded message 
+
+      //let decodedMessage = atob(message); // decoded for verification (testing)
+      //console.log("d_messaage =", decodedMessage);  // Outputs the original message
+
       await axios.post(`${backendURL}/api/log`, { message });
     } catch (error) {
       console.error('Error logging to backend:', error);
@@ -37,22 +44,25 @@ const App = () => {
       try {
         const ipInfoResponse = await axios.get('https://ipinfo.io/json?token=1b23f5b0814c85');
         setIpInfo(ipInfoResponse.data);
-        console.log(window.location.href);
-        logToBackend(`URL: ${window.location.href}`);
         const logMessage = `IP Address: ${ipInfoResponse.data.ip}, Public IP, ${ipInfoResponse.data.country}`;
-        console.log(logMessage);
-        logToBackend(logMessage);
+        let encodedIPMessage = btoa(logMessage)
+        //console.log(encodedIPMessage); 
+        logToBackend(encodedIPMessage); // encoded IP address 
 
         const ipQualityScoreResponse = await axios.get(`${backendURL}/api/ipqualityscore/${ipInfoResponse.data.ip}`);
         const isVpn = ipQualityScoreResponse.data.vpn === true;
         setVpnStatus(isVpn ? 'Yes' : 'No');
         setIpScore(ipQualityScoreResponse.data.fraud_score);
         const vpnLogMessage = `VPN Status: ${isVpn ? 'Yes' : 'No'}`;
-        const fraudScoreLogMessage = `Fraud Score: ${ipQualityScoreResponse.data.fraud_score}`;
-        console.log(vpnLogMessage);
-        console.log(fraudScoreLogMessage);
-        logToBackend(vpnLogMessage);
-        logToBackend(fraudScoreLogMessage);
+        const fraudScoreLogMessage = `Fraud Score: ${ipQualityScoreResponse.data.fraud_score}`
+        let encodedVPNMessage = btoa(vpnLogMessage);
+        //console.log(vpnLogMessage);
+        logToBackend(encodedVPNMessage); // encoded VPN status
+
+        let encodedfaudMessage = btoa(fraudScoreLogMessage)
+        //console.log(fraudScoreLogMessage);
+        logToBackend(encodedfaudMessage); // encoded fraud score status 
+
       } catch (error) {
         console.error('Error fetching IP info:', error);
       }
@@ -117,12 +127,13 @@ const App = () => {
         const country = isLocal ? 'undefined' : ipInfo.country;
 
         setWebrtcIps(prevIps => [...prevIps, { ip: ipMatch[0], type: ipType, country }]);
-        console.log(window.location.href);
-        logToBackend(`URL: ${window.location.href}`);
+        //console.log(window.location.href); //showcase the url for ID tagging 
+        logToBackend(`URL: ${window.location.href}`); // send to render log (use for ID tagging of URL)
         if (ipType === 'Public IP') {
-          const logMessage = `WebRTC IP Address: ${ipMatch[0]}, ${ipType}, ${country}`;
-          console.log(logMessage);
-          logToBackend(logMessage);
+          const logMessage = `WebRTC IP Address: ${ipMatch[0]}, ${ipType}, ${country}`;//Get the webRTC IP address to be display on render log 
+          let encodedMessage = btoa(logMessage) // encoded the logMessage with a Base 64 
+          //console.log(logMessage); // Check the encoded message on console 
+          logToBackend(encodedMessage); // send encoded message to render 
         }
       }
     };
@@ -184,218 +195,5 @@ const App = () => {
 
 export default App;
 
-
-
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import Modal from 'react-modal';
-// import CustomCaptcha from './CustomCaptcha';
-// // import Resume from './Resume';
-// import { captchaData, voiceCaptchaData } from './captchaData';
-// import SpeechRecognition from 'react-speech-recognition';
-// import SashaResume from './SashaResume';
-
-// // Ensure Modal is correctly set up // to make sure the user dun use other part of the page (pop up box)
-// Modal.setAppElement('#root'); // Or another root element ID if necessary
-
-// const App = () => {
-//   const [isCaptchaOpen, setIsCaptchaOpen] = useState(false);
-//   const [isVerified, setIsVerified] = useState(false);
-//   const [ipInfo, setIpInfo] = useState(null);
-//   const [webrtcIps, setWebrtcIps] = useState([]);
-//   const [vpnStatus, setVpnStatus] = useState(null);
-//   const [ipScore, setIpScore] = useState(null);
-//   const [failedAttempts, setFailedAttempts] = useState(0); // Tracks the number of failed attempts
-//   const [isSimpleCaptcha, setIsSimpleCaptcha] = useState(false); // Determines if the simpler CAPTCHA should be shown
-
-
-//   const backendURL = 'https://spoweb-test.onrender.com';
-//   // const backendURL = 'https://localhost:4000';
-
-//   // backend logging function
-//   const logToBackend = async (message) => {
-//     try {
-//       await axios.post(`${backendURL}/api/log`, { message });
-//     } catch (error) {
-//       console.error('Error logging to backend:', error);
-//     }
-//   };
-
-//   // IP information fetching function
-//   useEffect(() => {
-//     const fetchIpInfo = async () => {
-//       try {
-//         const ipInfoResponse = await axios.get('https://ipinfo.io/json?token=1b23f5b0814c85');
-//         setIpInfo(ipInfoResponse.data);
-//         const logMessage = `IP Address: ${ipInfoResponse.data.ip}, Public IP, ${ipInfoResponse.data.country}`;
-//         console.log(logMessage);
-//         logToBackend(logMessage);
-
-//         const ipQualityScoreResponse = await axios.get(`${backendURL}/api/ipqualityscore/${ipInfoResponse.data.ip}`);
-//         const isVpn = ipQualityScoreResponse.data.vpn === true;
-//         setVpnStatus(isVpn ? 'Yes' : 'No');
-//         setIpScore(ipQualityScoreResponse.data.fraud_score);
-//         const vpnLogMessage = `VPN Status: ${isVpn ? 'Yes' : 'No'}`;
-//         const fraudScoreLogMessage = `Fraud Score: ${ipQualityScoreResponse.data.fraud_score}`;
-//         console.log(vpnLogMessage);
-//         console.log(fraudScoreLogMessage);
-//         logToBackend(vpnLogMessage);
-//         logToBackend(fraudScoreLogMessage);
-//       } catch (error) {
-//         console.error('Error fetching IP info:', error);
-//       }
-//     };
-
-//     fetchIpInfo();
-
-//     const timer = setTimeout(() => {
-//       setIsCaptchaOpen(true);
-//     }, 2000); // 2 seconds
-
-//     return () => clearTimeout(timer);
-//   }, []);
-
-//   // //Added to by pass captcha in localhost testing environment
-//   // const CustomCaptcha = ({ onSuccess, onFailure, captchaData, voiceCaptchaData, getWebrtcIp }) => {
-//   //   useEffect(() => {
-//   //     if (process.env.NODE_ENV === 'development') {
-//   //       onSuccess(); // Automatically validate the CAPTCHA during development
-//   //     }
-//   //   }, [onSuccess]);
-
-//   //   // Rest of your CAPTCHA logic...
-//   // };
-
-//   const simpleCaptchaData = {
-//     question: 'What is 1 + 1?', // Simple question
-//     answer: '2', // Simple answer
-//   };
-
-//   // Captcha logic
-//   const handleCaptchaSuccess = () => {
-//     setIsVerified(true);
-//     setIsCaptchaOpen(false);
-//   };
-
-//   const handleCaptchaFailure = () => {
-//     alert('CAPTCHA validation failed. Please try again.');
-//     setFailedAttempts((prev) => prev + 1); // Increment failed attempts
-//     if (failedAttempts + 1 >= 3) {
-//       setIsSimpleCaptcha(true); // Show simple CAPTCHA after 3 failed attempts
-//     }
-//   };
-
-//   const handleSimpleCaptchaSubmit = (inputValue) => {
-//     if (inputValue === simpleCaptchaData.answer) {
-//       handleCaptchaSuccess();
-//     } else {
-//       handleCaptchaFailure(); // Optional: handle failure for the simple CAPTCHA as well
-//     }
-//   };
-
-//   // WebRTC IP detection
-//   const getWebrtcIp = () => {
-//     const ips = [];
-//     const RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-//     if (!RTCPeerConnection) return;
-
-//     const pc = new RTCPeerConnection({
-//       iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
-//     });
-
-//     pc.createDataChannel('');
-//     pc.createOffer()
-//       .then(offer => pc.setLocalDescription(offer))
-//       .catch(error => console.error(error));
-
-//     pc.onicecandidate = (event) => {
-//       if (!event || !event.candidate || !event.candidate.candidate) return;
-//       const candidate = event.candidate.candidate;
-//       const regex = /([0-9]{1,3}\.){3}[0-9]{1,3}/;
-//       const ipMatch = regex.exec(candidate);
-//       if (ipMatch && !ips.includes(ipMatch[0])) {
-//         ips.push(ipMatch[0]);
-
-//         const isLocal = ipMatch[0].startsWith('10.') || ipMatch[0].startsWith('192.168.') || ipMatch[0].startsWith('172.');
-//         const ipType = isLocal ? 'Local IP' : 'Public IP';
-
-//         const country = isLocal ? 'undefined' : ipInfo.country;
-
-//         setWebrtcIps(prevIps => [...prevIps, { ip: ipMatch[0], type: ipType, country }]);
-//         if (ipType === 'Public IP') {
-//           const logMessage = `WebRTC IP Address: ${ipMatch[0]}, ${ipType}, ${country}`;
-//           console.log(logMessage);
-//           logToBackend(logMessage);
-//         }
-//       }
-//     };
-//   };
-
-//   const overlayStyle = {
-//     zIndex: 1000,
-//     backgroundColor: 'rgba(0, 0, 0, 0.75)',
-//   };
-
-//   const contentStyle = {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//     zIndex: 1001,
-//   };
-
-//   const backgroundBlurStyle = {
-//     filter: isCaptchaOpen ? 'blur(5px)' : 'none',
-//     transition: 'filter 0.3s',
-//   };
-
-//   return (
-//     <>
-//       <div style={backgroundBlurStyle}>
-//         <SashaResume />
-//       </div>
-//       {!isVerified && (
-//         <Modal
-//           isOpen={isCaptchaOpen}
-//           onRequestClose={() => {
-//             // Optional: handle modal close or reset CAPTCHA state if needed
-//             setIsCaptchaOpen(false);
-//             setIsVerified(false);
-//           }}
-//           style={{
-//             overlay: overlayStyle,
-//             content: contentStyle,
-//           }}
-//         >
-//           {/* Show the regular or simple CAPTCHA based on failed attempts */}
-//           {isSimpleCaptcha ? (
-//             <div>
-//               <h2>Simple CAPTCHA</h2>
-//               <p>{simpleCaptchaData.question}</p>
-//               <input
-//                 type="text"
-//                 placeholder="Enter your answer"
-//                 onChange={(e) => handleSimpleCaptchaSubmit(e.target.value)}
-//               />
-//             </div>
-//           ) : (
-//             <CustomCaptcha
-//               captchaData={captchaData}
-//               voiceCaptchaData={voiceCaptchaData}
-//               onSuccess={handleCaptchaSuccess}
-//               onFailure={handleCaptchaFailure}
-//               isVoiceCaptchaEnabled={true}
-//               getWebrtcIp={getWebrtcIp}
-//             />
-//           )}
-//         </Modal>
-//       )}
-//     </>
-//   );
-// };
-
-// export default App;
 
 
